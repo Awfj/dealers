3eec386e4d42ce31e4ed0535f0d436628a750839
Alexey Kokhanovsky <awfj837@gmail.com>
2019-05-22 13:17:32 +0300

    Updates

---
+++ precache-manifest.811d15bbe86a0777dcd863d2e86cadc3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee8fee0547d08dfdc58d88969408773",
+    "url": "/dealers/index.html"
+  },
+  {
+    "revision": "46ba121e45e93b521f0b",
+    "url": "/dealers/static/css/main.bb8e8fb4.chunk.css"
+  },
+  {
+    "revision": "7c0bb8f0e39767f3a30e",
+    "url": "/dealers/static/js/2.b2a97c03.chunk.js"
+  },
+  {
+    "revision": "46ba121e45e93b521f0b",
+    "url": "/dealers/static/js/main.2d046f3c.chunk.js"
+  },
+  {
+    "revision": "bb00f40d44f73bc5dd7a",
+    "url": "/dealers/static/js/runtime~main.6ae02ff1.js"
+  },
+  {
+    "revision": "584046f0e0789aeb89ed229177b61c46",
+    "url": "/dealers/static/media/banner-home.584046f0.png"
+  },
+  {
+    "revision": "762c712cc02ab8d3d41fe54a54f9e56e",
+    "url": "/dealers/static/media/cover-block-1.762c712c.png"
+  },
+  {
+    "revision": "1b1436fc49fab02727bc0cf15d14bc69",
+    "url": "/dealers/static/media/cover-block-2.1b1436fc.png"
+  },
+  {
+    "revision": "f3437c81804dd00374705b877b048181",
+    "url": "/dealers/static/media/product-1.f3437c81.png"
+  },
+  {
+    "revision": "a6934bb1f8cedc64adc567f6384115b2",
+    "url": "/dealers/static/media/product-2.a6934bb1.png"
+  },
+  {
+    "revision": "4bf9a050e1f80c2084c1a1f587a316a5",
+    "url": "/dealers/static/media/product-3.4bf9a050.png"
+  }
+]);
