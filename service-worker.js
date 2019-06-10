b313b16d9677c2ffcbd5c107cc35058ddd7d2505
Alexey Kokhanovsky <awfj837@gmail.com>
2019-06-10 18:36:00 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/dealers/precache-manifest.95a9794bb3e30a667634367fca51cec5.js"
+  "/dealers/precache-manifest.21c3376df27c5b6bf504ef2c06b56798.js"
 );
 
 self.addEventListener('message', (event) => {
